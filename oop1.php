07699506e3e421721a01770c4479cf87aa3da145
karim <namekar09@gmail.com>
2024-08-29 16:22:32 +0100

    second_change

--- oop1.php
+++ oop1.php
@@ -1,30 +1,54 @@
 <?php
 
     class User {
-        public $username = 'ryu';
-        public $email = 'namekar09@gmail.com';
+        public $username;
+        private  $email;
         public function addFriend(){
             
             return "$this->email added a new friend";
         }
-
+        public function __construct(public $theUser,public $TheEmail){
+            $this->username = $theUser;
+            $this->email = $TheEmail;
+        }
+        //GETTER
+        public function getEmail(){
+            return "$this->email added a new photo";
+        }
+        public function setEmail($email){
+            if(strpos($email,'@') >-1){
+                $this->email = $email;
+            }
+        }
+    }
+    class adminUser extends User {
 
     }
-    $UserOne = new User ();
-    $UserTwo = new User();
+    $UserOne = new User ('mario','mario@gmail.com');
+    $UserTwo = new User('luigi','luigi@gmail.com');
+    $UserThree = new adminUser('yoshi','yoshit@gmail.com');
+    echo $UserThree->username . '<br>';
+    echo $UserThree->getEmail() . '<br>';
+
+
+    $UserOne->setEmail('yoshi@gmail.com');
+    echo $UserOne->getEmail() .'<br>';
+    echo $UserTwo->getEmail() .'<br>';
+
+
+
+
+
     echo get_class($UserOne) . '<br>';
     echo $UserOne->username . '<br>';
-
-    
-    echo $UserOne->addFriend() . '<br>';
+    echo $UserOne->getEmail() . '<br>';
+    echo $UserTwo->getEmail() . '<br>';
     print_r(get_class_vars('User'));
     print_r(get_class_methods('User'));
     echo get_class($UserTwo) . '<br>';
-    
     echo $UserTwo->addFriend() . '<br>';
-    $UserTwo->username = 'yoshi';
-    $UserTwo->email = 'karim@gmail.com';
     echo $UserTwo->username . '<br>';
+    
     
     ?>
 <!DOCTYPE html>
